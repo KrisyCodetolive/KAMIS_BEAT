18b4e4eb7b0e6c1541905320ab8741d15b23cfda
Kris <Kris.005Kouame.2024@gmail.com>
2024-10-07 01:40:23 -0500

    firstPage

--- src/bodyPage/Acceuil.jsx
+++ src/bodyPage/Acceuil.jsx
@@ -1,25 +1,115 @@
-
+import { afro, rap, trap, sound, Arrow, Arrown, Send, Send_com } from '../bodyPage/Import'
 
 export function Acceuil(){
+
+    
     return (
         <div className="Body">
 
             <section className="Pub">Publicité</section>
 
 
-            <section className="recommande">recommande</section>
+            <section className="recommande">
+                {/* <h1 className="reh1">Recommandé</h1> */}
+
+                <div className="conteneur-rec">
+                    <img src={Arrow} className='Arrow' alt="" />
+                    <div className="conteneur-Aff">
+
+                        
+
+                        <div className="secondaire1">
+                            <img src={rap} className='second-1' alt="" />
+
+                        </div>
+
+                        <div className="Aff-main">
+
+                            <div className="lueur">
+                            </div>
+
+                            <img src={trap} className='Main-Aff' alt="" />
+
+                            <div className="title">
+                                <h1>Faux coeur</h1>
+                                <h2>140 BPM . trap ragga</h2>
+                                <div >
+                                    <img src={sound} className="sound" alt="" />
+                                </div>
+                            </div>
 
 
-            <section className="instrumental">instrumental</section>
+                        </div>
+
+                        
+                        <div className="secondaire2">
+
+                            <img src={afro} className='second-2' alt="" />
+                        </div>
+                    </div>
+                    <img src={Arrown} className='Arrow'  alt="" />
+                </div>
+            
+            </section>
 
 
-            <section className="message">message</section>
+            <section className="instrumental"><h1 className="reh1">Instrumental</h1></section>
+
+            
+            <section className="message">
+
+                <h1 className='title-mess'>Contactez-nous</h1>
+                <form action="#" className="contactez">
+
+                    <div className='namemail'>
+                        <input type="text" className="input name" placeholder="Nom"/>
+                        <input type="text" className="input email"  placeholder="Email"/>
+                    </div>
+
+                    <input type="text" className="input"  placeholder="Sujet"/>
+                    <input type="text" className="input"  placeholder="Message"/>
+                    
+                    <Button/>
+                </form>
+            </section>
 
 
-            <section className="commentaire">commentaire</section>
+            <section className="commentaire">
+                <h1 className="title-com">Commentaire</h1>
+
+                <form action="#" className='form_com'>
+                    <input type="text" placeholder='Laissez-nous un commentaire' className='input-com'/>
+                    <img src={Send_com} className="Send_com" alt="" />
+                </form>
+                <div className="Box-com">
+
+                </div>
+                
+            </section>
 
 
             
         </div>
+    );
+}
+
+
+export function btnPlayer(){
+
+    return (
+            <div className="btmplayer">
+
+            </div>
+    );
+}
+
+
+export function Button(){
+
+    return(
+        <button type="submit" className="connexion send">
+            <h1>Envoyer</h1>
+            <img src={Send} className='IconeSend' alt="" />
+        </button>
     );
 }
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+
 import Kamis from '../Resset/Icone/Narbar_icone/Kamis.svg';
+import Youtub from '../Resset/Icone/Footer_icone/Youtub.svg';
+import Tiktok from '../Resset/Icone/Footer_icone/Tiktok.svg';
+import Instagram from '../Resset/Icone/Footer_icone/Instagram.svg';
 
 export default function Footer(){
 
@@ -8,27 +11,27 @@ export default function Footer(){
             <footer className='footer'>
                 
                 <div className='footer-logo'>
-                    <img src={Kamis} alt="logo" />
+                    <img src={Kamis} className= "logo" alt="logo" />
                 </div>
 
                 <div className="footer-menu1">
                     <ul className="menu-1">
-                        <li className="menu1"></li>
-                        <li className="menu2"></li>
-                        <li className="menu3"></li>
-                        <li className="menu4"></li>
+                        <li className="menu1">Acceuil</li>
+                        <li className="menu2">Recevoir mon cadeau</li>
+                        <li className="menu3">Contacter nous</li>
+                        <li className="menu4">Qui suis-je</li>
                     </ul>
                     <div className="link-contact">
-                            <img src="" alt="" />
-                            <img src="" alt="" />
-                            <img src="" alt="" />
+                            <img src={Youtub} className='logo-footer' alt="" />
+                            <img src={Tiktok} className='logo-footer' alt="" />
+                            <img src={Instagram} className='logo-footer' alt="" />
                     </div>
                 </div>
 
                 <div className="footer-menu2">
                     <ul className="menu-2">
-                        <li className="menu2-1"></li>
-                        <li className="menu2-2"></li>
+                        <li className="menu2-1">Privacy policy</li>
+                        <li className="menu2-2">Licence</li>
                     </ul>
                 </div>
                 
